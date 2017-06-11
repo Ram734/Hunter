77f752b923b137b30804d12c73b08c5b56b0cc54
Ram734 <prasannaking48@gmail.com>
2017-06-11 21:08:00 +0530

    Add files via upload

---
+++ unsorted.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class unsorted
+{
+	public static void main(String a[])
+	{
+		for(int i=0;i<a.length;i++)
+			{
+				for(int j=0;j<a.length;j++)
+				{
+					if(i==j)
+					{
+					}
+                 else
+				 {
+					 if(a[i].equals(a[j]))
+					 {
+						 a[i]="*";
+						 a[j]="*";
+					 }
+				 }
+				}
+			}
+			for(int k=0;k<a.length;k++)
+			{
+				if(a[k]!="*")
+				{
+					System.out.print(a[k]+" ");
+				}
+			}
+	}
+}
